5fe1d351679839104ee6f3abe8172a2ea954e2d5
bananakodira <ana.urlic@gmail.com>
2022-03-31 17:05:52 +0200

    Added CSS normalize	new file:   css/normalize.css Added CSS reset 	new file:   css/reset.css Added CSS file 	new file:   css/style.css Added index file	new file:   index.html Add Square Sum 8kyu codewars file	new file:   js/app.js Added Count the Sheep js file	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Complete the square sum function so that it squares each number passed into 
+//it and then sums the results together.
+
+//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+//P numbers, strings, special chars? 
+//R sum of squared results
+//E
+
+// Make  a function that takes in an array 
+//function squareSum (arr){
+////map through array amnd square each element
+//return arr.map(num => num **2).reduce ((acc,c) => acc + c,0)
+//}
+//
+//console.log(squareSum([1,2,5]),30 )
+//console.log(squareSum([2,5,8]),93 )
+//console.log(squareSum([3,3,3]),27 )
+

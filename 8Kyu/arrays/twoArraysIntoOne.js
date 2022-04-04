1891b4a0ac0df4d3b75cf68a8d4fbed96519cf90
Ana Urlic <ana.urlic@gmail.com>
2022-04-04 11:49:30 +0200

    Added a solution to Two Arrays into One

--- 8Kyu/arrays/twoArraysIntoOne.js
+++ 8Kyu/arrays/twoArraysIntoOne.js
@@ -6,12 +6,6 @@
 
 // Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
 
-// Examples (input -> output)
-// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
 
 function mergeArrays(arr1, arr2) {
     let twoArrays = arr1.concat(arr2);

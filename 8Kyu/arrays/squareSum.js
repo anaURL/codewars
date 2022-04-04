9b9aca33563949fcbe454261737f76fa2f23e573
Ana Urlic <ana.urlic@gmail.com>
2022-04-04 11:50:49 +0200

    Added a solution for Square Sum

--- 8Kyu/arrays/squareSum.js
+++ 8Kyu/arrays/squareSum.js
@@ -3,16 +3,10 @@
 
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-//P numbers, strings, special chars? 
-//R sum of squared results
-//E
-
 // Make  a function that takes in an array 
-//function squareSum (arr){
-////map through array amnd square each element
-//return arr.map(num => num **2).reduce ((acc,c) => acc + c,0)
-//}
-//
-//console.log(squareSum([1,2,5]),30 )
-//console.log(squareSum([2,5,8]),93 )
-//console.log(squareSum([3,3,3]),27 )
+function squareSum (arr){
+//map through array amnd square each element
+return arr.map(num => num **2).reduce ((acc,c) => acc + c,0)
+}
+
+
